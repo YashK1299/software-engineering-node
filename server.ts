b75733d258fa000770f4d68ad709f929284f1769
Yash Kothari <kothariy3@gmail.com>
2022-04-08 09:40:58 -0400

    Keeping Track of User Related Data

--- server.ts
+++ server.ts
@@ -14,71 +14,72 @@
  * service
  */
 import dotenv from 'dotenv';
-import express, {Request, Response} from 'express';
-import UserController from './controllers/UserController';
-import TuitController from './controllers/TuitController';
-import LikeController from './controllers/LikeController';
-import MessageController from './controllers/MessageController';
-import FollowController from './controllers/FollowController';
-import BookmarkController from './controllers/BookmarkController';
-import mongoose from 'mongoose';
-import AuthenticationController from './controllers/AuthenticationController';
-const cors = require("cors");
+ import express, {Request, Response} from 'express';
+ import UserController from "./controllers/UserController";
+ import TuitController from "./controllers/TuitController";
+ import LikeController from "./controllers/LikeController";
+ import MessageController from './controllers/MessageController';
+ import FollowController from './controllers/FollowController';
+ import BookmarkController from './controllers/BookmarkController';
+ import AuthenticationController from "./controllers/AuthenticationController";
+ import mongoose from "mongoose";
+ const cors = require("cors");
+ const session = require("express-session");
 
-// To read the config file
-dotenv.config();
-
-// Connnect to DB
-const DB_USERNAME = process.env.DB_USERNAME;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const connectionString = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.ei10j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-mongoose.connect(connectionString)
-
-const app = express();
-
-// Securing user session
-const session = require("express-session");
-let sess = {
-    secret: process.env.EXPRESS_SESSION_SECRET,
-    saveUninitialized: true,
-    resave: true,
-    cookie: {
-        sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax',
-        secure: process.env.NODE_ENV === "production",
-    }
-}
-if (process.env.NODE_ENV === 'production') {
-    app.set('trust proxy', 1) // trust first proxy
-    sess.cookie.secure = true // serve secure cookies
-}
-
-app.use(cors({
+ // To read the config file
+ dotenv.config();
+ 
+ // build the connection string
+ const DB_USERNAME = process.env.DB_USERNAME;
+ const DB_PASSWORD = process.env.DB_PASSWORD;
+ const connectionString = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.ei10j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;// connect to the database
+ mongoose.connect(connectionString);
+ 
+ const app = express();
+ app.use(cors({
      credentials: true,
-     origin: 'http://localhost:3000'
-    }));
-app.use(session(sess));
-app.use(express.json());
+     origin: process.env.CORS_ORIGIN
+ }));
+ 
+ let sess = {
+     secret: process.env.EXPRESS_SESSION_SECRET,
+     saveUninitialized: true,
+     resave: true,
+     cookie: {
+         sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax',
+         secure: process.env.NODE_ENV === "production",
+        // secure: false
+     }
+ }
+ 
+ if (process.env.NODE_ENV === 'production') {
+     app.set('trust proxy', 1) // trust first proxy
+     sess.cookie.secure = true // serve secure cookies
+ }
+ 
+ app.use(session(sess));
+ app.use(express.json());
 
-app.get('/hello', (req: Request, res: Response) =>
-    res.send('Hello World!'));
+ app.get('/hello', (req: Request, res: Response) =>
+     res.send('Hello World!'));
+ 
+ app.get('/', (req: Request, res: Response) =>
+     res.send('Welcome!'));
+ 
+ app.get('/add/:a/:b', (req: Request, res: Response) =>
+     res.send(req.params.a + req.params.b));
 
-app.get('/', (req: Request, res: Response) =>
-    res.send('Welcome'));
-
-app.get('/add/:a/:b', (req: Request, res: Response) =>
-    res.send(req.params.a + req.params.b));
-
-// Setting up the instance for each RESTfull API
-const userController = UserController.getInstance(app);
-const tuitController = TuitController.getInstance(app);
-const likeController = LikeController.getInstance(app);
-const followController = FollowController.getInstance(app);
-const messageController = MessageController.getInstance(app);
-const bookmarkController = BookmarkController.getInstance(app);
-AuthenticationController(app);
-
-/**
- * Start a server listening at port 4000 locally
- * but use environment variable PORT on Heroku if available.
- */
-app.listen(process.env.PORT);
+     // Setting up the instance for each RESTfull API
+ const userController = UserController.getInstance(app);
+ const tuitController = TuitController.getInstance(app);
+ const likesController = LikeController.getInstance(app);
+ const followController = FollowController.getInstance(app);
+ const messageController = MessageController.getInstance(app);
+ const bookmarkController = BookmarkController.getInstance(app);
+ AuthenticationController(app);
+ /**
+  * Start a server listening at port 4000 locally
+  * but use environment variable PORT on Heroku if available.
+  */
+ const PORT = 4000;
+ app.listen(process.env.PORT || PORT);
